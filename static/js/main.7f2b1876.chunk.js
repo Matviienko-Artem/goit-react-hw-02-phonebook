0c48dcb5a9edef35f27f17205058eead3197b04a
Matviienko Artem <k2141092mav@gmail.com>
2021-03-14 19:06:08 +0200

    Updates

---
+++ static/js/main.7f2b1876.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(e,t,o){"use strict";o.r(t);var n=o(0),c=o.n(n),r=o(3),h=o.n(r),i=(o(8),o(1));var s=function(){return Object(i.jsx)("h2",{children:"Hello"})};h.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(s,{})}),document.getElementById("root"))}},[[10,1,2]]]);
+//# sourceMappingURL=main.7f2b1876.chunk.js.map
